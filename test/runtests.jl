e9d6b89b638a9c0bee2d607c2007eff1f39fb9fe
Tim Holy <tim.holy@gmail.com>
2020-01-18 12:48:00 -0600

    Remove ZeroTo, switch to Project, and release 1.0.0

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,6 @@ using Test
 
 include("zerorange.jl")
 include("urange.jl")
-include("zeroto.jl")  # deprecated
 
 @test intersect(ZeroRange(4), URange(-1,2)) === 0:2
 @test ZeroRange(6)[URange(2,4)] === 1:3
